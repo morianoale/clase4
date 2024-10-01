991bc685aa5e9aaaf7182f6fa2518942df643391
morianoale <alexandra.moriano@iestpmonfortianos.edu.pe>
2024-09-30 20:40:06 -0500

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,6 @@
+let elementos=document.getElementsByClassName("miClase");
+for(let i=0; i<elementos.length;i++){
+    elementos[i].style.color="green";
+    elementos[i].style.fontFamily="Arial";
+    elementos[i].style.backgroundColor="black";
+}
